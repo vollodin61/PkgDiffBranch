5980f857662cd47dec246c33249213b96984400a
ilia_volodin <vollodin61@gmail.com>
2024-10-01 16:55:34 +0300

    add get_packages_data, использует fetch_packages, чтобы сделать запросы в 2 ветки и вернуть tuple с данными

--- compare_packages.py
+++ compare_packages.py
@@ -43,3 +43,21 @@ async def fetch_packages(url: str, session: ClientSession, branch: str, arch: st
                 await asyncio.sleep(5)  # Изменено на асинхронный sleep
             else:
                 raise RuntimeError(f"Не удалось установить соединение после {retries} попыток")
+
+
+async def get_packages_data(url: str, branch1: str, branch2: str, arch: str) -> tuple[
+    list[dict[str, str]], list[dict[str, str]]]:
+    """
+    Получает данные для двух веток и возвращает их.
+
+    :param url: Базовый URL для API.
+    :param branch1: Первая ветка для запроса.
+    :param branch2: Вторая ветка для запроса.
+    :param arch: Архитектура пакетов.
+    :return: Кортеж из списков пакетов для обеих веток.
+    """
+    async with ClientSession() as session:
+        packages1 = await fetch_packages(url, session, branch1, arch)
+        packages2 = await fetch_packages(url, session, branch2, arch)
+        return packages1, packages2
+
